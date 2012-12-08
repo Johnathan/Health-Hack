fd9e6884f2d49c8d8e071aff8c333640db3f53c5
Pete Hawkins <pete@phawk.co.uk>
2012-12-08 13:26:11 +0000

    Add local configs

---
+++ application/config/local/application.php
@@ -0,0 +1,58 @@
+<?php
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Application URL
+    |--------------------------------------------------------------------------
+    |
+    | The URL used to access your application without a trailing slash. The URL
+    | does not have to be set. If it isn't, we'll try our best to guess the URL
+    | of your application.
+    |
+    */
+
+    'url' => '',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Asset URL
+    |--------------------------------------------------------------------------
+    |
+    | The base URL used for your application's asset files. This is useful if
+    | you are serving your assets through a different server or a CDN. If it
+    | is not set, we'll default to the application URL above.
+    |
+    */
+
+    'asset_url' => '',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Application Index
+    |--------------------------------------------------------------------------
+    |
+    | If you are including the "index.php" in your URLs, you can ignore this.
+    | However, if you are using mod_rewrite to get cleaner URLs, just set
+    | this option to an empty string and we'll take care of the rest.
+    |
+    */
+
+    'index' => '',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Application Key
+    |--------------------------------------------------------------------------
+    |
+    | This key is used by the encryption and cookie classes to generate secure
+    | encrypted strings and hashes. It is extremely important that this key
+    | remains secret and it should not be shared with anyone. Make it about 32
+    | characters of random gibberish.
+    |
+    */
+
+    'key' => 'ZuSfqcq2WlCkajjMwGaZhhiZneYnbTnr',
+
+);
