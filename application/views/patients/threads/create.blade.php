13043b8f8449d52d928abcba2d8190e6d2cc0df1
Pete Hawkins <pete@phawk.co.uk>
2012-12-08 20:05:13 +0000

    Make formy floaty thing

--- application/views/patients/threads/create.blade.php
+++ application/views/patients/threads/create.blade.php
@@ -2,35 +2,40 @@
 
 @section('content')
 
-    @include('patients._profile')
+    <div class="row">
+        <div class="span3">
+            @include('patients._profile')
+        </div><!-- end .span3 -->
 
-    <h2>Post a new issue</h2>
+        <div class="span9">
+            <h2>Post a new issue</h2>
 
-    {{Form::open()}}
-    	{{Form::label( 'title', 'Title' )}}
-    	{{Form::text( 'title', Input::old( 'title' ) )}}
-    	@if( $errors->has( 'title' ) )
-    		<p>{{$errors->first( 'title' )}}</p>
-    	@endif
-    	<br />
+            {{Form::open()}}
+            	{{Form::label( 'title', 'Title' )}}
+            	{{Form::text( 'title', Input::old( 'title' ) )}}
+            	@if( $errors->has( 'title' ) )
+            		<p>{{$errors->first( 'title' )}}</p>
+            	@endif
+            	<br />
 
-        {{Form::label( 'message', 'Message' )}}
-        {{Form::textarea( 'message', Input::old( 'message' ) )}}
-        @if( $errors->has( 'message' ) )
-            <p>{{$errors->first( 'message' )}}</p>
-        @endif
-        <br />
+                {{Form::label( 'message', 'Message' )}}
+                {{Form::textarea( 'message', Input::old( 'message' ) )}}
+                @if( $errors->has( 'message' ) )
+                    <p>{{$errors->first( 'message' )}}</p>
+                @endif
+                <br />
 
-        <label><input type="checkbox" name="urgency" value="urgent"> <strong>This is urgent</strong></label>
+                <label><input type="checkbox" name="urgency" value="urgent"> <strong>This is urgent</strong></label>
 
-        <h4>Disciplines</h4>
+                <h4>Disciplines</h4>
 
-        <label><input type="checkbox" name="discipline[]" value="Bleh"> Cardiovascular</label>
-        <label><input type="checkbox" name="discipline[]" value="Bleh"> Neurological</label>
-        <label><input type="checkbox" name="discipline[]" value="Bleh"> Psychiatric</label>
-        <label><input type="checkbox" name="discipline[]" value="Bleh"> Respiratory</label>
+                <label><input type="checkbox" name="discipline[]" value="Bleh"> Cardiovascular</label>
+                <label><input type="checkbox" name="discipline[]" value="Bleh"> Neurological</label>
+                <label><input type="checkbox" name="discipline[]" value="Bleh"> Psychiatric</label>
+                <label><input type="checkbox" name="discipline[]" value="Bleh"> Respiratory</label>
 
-    	{{Form::submit( 'Submit' )}}
-    {{Form::close()}}
-
+            	{{Form::submit( 'Submit', array('class' => 'btn btn-primary') )}}
+            {{Form::close()}}
+        </div><!-- end .span9 -->
+    </div><!-- end .row -->
 @endsection
