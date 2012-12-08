c15aa2abcd30c34293c6d7ebd100b5dc0e04a1ef
xz <robert@craftydevil.co.uk>
2012-12-08 20:16:03 +0000

    refined color

--- application/views/patients/index.blade.php
+++ application/views/patients/index.blade.php
@@ -3,20 +3,18 @@
 @section('content')
     <!-- <h2>My patients <small><a href="{{ URL::to('/patients/new') }}">Add Patient</a></small></h2> -->
     
-    <header class="instructions">
-    	<h1>Patients</h1>
-    	<h2>Instructions go here</h2>
+    <header class="header-instructions">
+    	<h1 class="instruction">Patients</h1>
+    	<h2 class="supporting-text">Browse patients currently in Second Opinion</h2>
     </header>
 
 	@if( isset($patients) && count($patients) > 0 )
 		<ul class="patient-cards">
 		@foreach( $patients as $patient )
 			<li class="patient-card">
-				
-					<div class="avatar-frame">
-						<img src="/img/patient.jpg" />
-					</div>
-				
+				<div class="avatar-frame">
+					<img src="/img/patient.jpg" />
+				</div>
 				
 				<div class="patient-card-right">
 					<a href="{{ URL::to('/patients/'.$patient->id) }}"><h3 class="card-title">{{$patient->name}}</h3></a>
