1c5e3051f6e2aa43ce9e722020c25f67c85195cd
Pete Hawkins <pete@phawk.co.uk>
2012-12-08 17:36:52 +0000

    Make posting a message work

--- application/controllers/threads.php
+++ application/controllers/threads.php
@@ -13,15 +13,4 @@ class Threads_Controller extends Base_Controller
             ->with( 'patient', $patient );
     }
 
-    public function post_show($patient_id, $thread_id) {
-        $thread = Auth::user()->patients()->where_id( $patient_id )->first()->threads()->where_id( $thread_id )->first();
-        $patient = $thread->patient()->first();
-
-        // Submit a new message
-
-        return View::make('patients.threads.show')
-            ->with( 'thread', $thread )
-            ->with( 'patient', $patient );
-    }
-
 }
--- application/views/patients/threads/messages/_list-item.blade.php
+++ application/views/patients/threads/messages/_list-item.blade.php
@@ -1,6 +1,6 @@
 <section class="message-metadata">
     <h4>{{ $message->user()->first()->name }}</h4>
-    <time>{{ date("jS F Y", $message->created_at) }}</time>
+    <time>{{ date("jS F Y", strtotime($message->created_at)) }}</time>
 </section>
 <section class="message-body">
     {{ $message->message }}
