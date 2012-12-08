fb6addd5b43f670b2ca19528deeabe9a32d9b351
xz <robert@craftydevil.co.uk>
2012-12-08 20:55:16 +0000

    starting threads

--- application/views/patients/_profile.blade.php
+++ application/views/patients/_profile.blade.php
@@ -1,10 +1,13 @@
 <section class="patient-profile">
     <div class="patient-img">
         <span class="avatar-icon">
-            <img src="{{ $patient->avatar }}" alt="{{ $patient->name }}">
+            <div class="avatar-frame">
+					<img src="/img/patient.jpg">
+				</div>
         </span>
     </div>
-    <h2>{{ $patient->name }}</h2>
+    <h2 class="patient-profile">{{ $patient->name }}</h2>
+    
     <h3>Care of {{ $patient->user()->first()->name }}</h3>
 
     <div class="patient-address">
