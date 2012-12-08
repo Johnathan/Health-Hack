6ace5c5fbb980e9af534851ef63b1a692af7da78
xz <robert@craftydevil.co.uk>
2012-12-08 18:47:13 +0000

    more stuff

--- application/views/patients/index.blade.php
+++ application/views/patients/index.blade.php
@@ -4,13 +4,24 @@
     <h2>My patients <small><a href="{{ URL::to('/patients/new') }}">Add Patient</a></small></h2>
 
 	@if( isset($patients) && count($patients) > 0 )
-		<ul>
+		<ul class="patient-cards">
 		@foreach( $patients as $patient )
 			<li class="patient-card">
-				<div class="avatar-frame">
-					<img src="/img/patient.jpg" />
+				
+					<div class="avatar-frame">
+						<img src="/img/patient.jpg" />
+					</div>
+				
+				
+				<div class="patient-card-right">
+					<a href="{{ URL::to('/patients/'.$patient->id) }}"><h3 class="card-title">{{$patient->name}}</h3></a>
+					
+					<hr class="card-rule" />
+					
+					<h4 class="card-footer">Open Issue</h4>
 				</div>
-			<a href="{{ URL::to('/patients/'.$patient->id) }}">{{$patient->name}}</a>
+				
+				
 			</li>
 		@endforeach
 		</ul>
