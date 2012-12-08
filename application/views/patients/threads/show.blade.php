0cbf3d0e8479d78034d2f9f20506da7fbb28adef
Pete Hawkins <pete@phawk.co.uk>
2012-12-08 19:45:03 +0000

    Add messages field to thread

--- application/views/patients/threads/show.blade.php
+++ application/views/patients/threads/show.blade.php
@@ -3,22 +3,29 @@
 @section('content')
     @include('patients._profile')
 
-    <h2>
-        {{$thread->title}}
-        <small>Last updated: {{ date("H:i jS M Y", strtotime($thread->updated_at)) }}</small>
-    </h2>
+    <section class="thread-description">
+        <h2>
+            {{$thread->title}}
+            <small>Last updated: {{ date("H:i jS M Y", strtotime($thread->updated_at)) }}</small>
+        </h2>
 
-    @include('patients.threads.messages._form')
+        <p>{{ nl2br($thread->message) }}</p>
+    </section>
 
-    @if( $thread->messages()->get() )
-    <ol class="thread-messages">
-        @foreach( $thread->messages()->get() as $message )
-        <li>
-            @include('patients.threads.messages._list-item')
-        </li>
-        @endforeach
-    </ol>
-    @else
-    <p>No messages yet</p>
-    @endif
+    <section class="messages">
+        @include('patients.threads.messages._form')
+
+        @if( $thread->messages()->get() )
+        <ol class="thread-messages">
+            @foreach( $thread->messages()->get() as $message )
+            <li>
+                @include('patients.threads.messages._list-item')
+            </li>
+            @endforeach
+        </ol>
+        @else
+        <p>No messages yet</p>
+        @endif
+    </section>
+
 @endsection
