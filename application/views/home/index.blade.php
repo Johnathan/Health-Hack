d5a296e6b7e11f4eb7b03dc28872645371800687
David HUghes <itsdavidhughes@gmail.com>
2012-12-08 20:23:00 +0000

    Added elevator pitch to public homepage, including CSS for typography

--- application/views/home/index.blade.php
+++ application/views/home/index.blade.php
@@ -1,5 +1,10 @@
 @layout('layouts.frontend')
 
+
+
 @section('content')
-    <h2>Home</h2>
+<div class="elevator">
+    <h1>A collaboration platform for healthcare professionals</h1>
+    <h2>Helping you make better decisions with timely information from trusted peers.</h2>
+</div>
 @endsection
